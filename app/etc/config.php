23c6820d88607973e5577d752019d0603f2d609b
Iago F. <n3rdydzn@hotmail.com>
2025-12-03 17:51:27 -0300

    Add custom footer and MSP DevTools integration

--- app/etc/config.php
+++ app/etc/config.php
@@ -377,6 +377,8 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magento_WishlistSampleData' => 1,
+        'MSP_Common' => 1,
+        'MSP_DevTools' => 1,
         'MarkShust_DisableTwoFactorAuth' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeCustomerBalance' => 1,
