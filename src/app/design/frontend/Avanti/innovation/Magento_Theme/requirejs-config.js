5c607af1b17abb0ba74f4c8e2c780789235f25bb
Iago F. <n3rdydzn@hotmail.com>
2026-01-12 17:57:37 -0300

    add: quantity stepper, share button and style the PDP page

--- src/app/design/frontend/Avanti/innovation/Magento_Theme/requirejs-config.js
+++ src/app/design/frontend/Avanti/innovation/Magento_Theme/requirejs-config.js
@@ -2,11 +2,15 @@ var config = {
     map: {
         '*': {
             footerAccordion: 'js/footer-accordion',
-            slickSlider: 'js/slick-slider'
+            slickSlider: 'js/slick-slider',
+            qtyStepper: 'js/qty-stepper',
+            productShare: 'js/product-share'
         }
     },
     deps: [
         'footerAccordion',
-        'slickSlider'
+        'slickSlider',
+        'qtyStepper',
+        'productShare'
     ]
 };
